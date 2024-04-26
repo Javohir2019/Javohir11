2eab8292718cae320c039efe224495e038195e1f
unknown <norbekovjavohir27@gmail.com>
2024-04-26 13:52:14 +0500

    1 - comment

---
+++ news/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import News
+from .models import Category
+from .models import *
+
+
+admin.site.register(News)
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Comment)
+
+
+
+
+
+# Register your models here.
---
+++ news/f.py
@@ -0,0 +1,5 @@
+def f(a):
+    b = a + 10
+    f(1,2)
+    return b
+print(f(10))
